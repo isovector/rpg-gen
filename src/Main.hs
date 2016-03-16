9b01dd541831a0c88b5a0429d80a82365b5513c6
Sandy Maguire <sandy@sandymaguire.me>
2016-03-16 08:17:55 -0700

    tree surroundings generation

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,8 @@ module Main where
 import Preface
 
 import Debug.Trace
+import Data.List (sortBy)
+import Data.Ord (comparing)
 import Control.Monad.Writer
 import Control.Arrow ((***))
 import FRP.Helm
@@ -27,32 +29,67 @@ data Player = Player
 
 data City = City
     { roads :: [Road]
-    , buildings :: ()
+    , surroundings :: [Form]
     }
 
 data Road = Road (Double, Double) (Double, Double) Color
 
 cityGen :: Some City
-cityGen = City <$> roadsGen <*> pure ()
-
-roadsGen :: Some [Road]
-roadsGen = do
+cityGen = do
     width  <- uniformIn (100, 200)
     height <- uniformIn (100, 200)
+    City <$> roadsGen width height <*> surroundingsGen width height
+
+
+surroundingsGen :: Double -> Double -> Some [Form]
+surroundingsGen width' height' = do
+    let width = width' * 2
+        height = height' * 2
+    numTrees <- uniformIn (50, 150)
+    trees <- listOf numTrees treeGen
+
+    forM trees $ \tree -> do
+        (xoffset', yoffset') <- uniformly [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        let xoffset = xoffset' * width'
+            yoffset = yoffset' * height'
+        xspread <-
+            if xoffset' == 0
+               then uniformIn (-width / 2, width / 2)
+               else uniformIn (-50, 50)
+        yspread <-
+            if yoffset' == 0
+               then (subtract $ height/2) <$> uniformIn (0, height)
+               else uniformIn (-50, 50)
+        return $ move (xoffset + xspread, yoffset + yspread) tree
+
+treeGen :: Some Form
+treeGen = do
+    color <- rgb <$> uniformIn (0.2, 0.4) <*> uniformIn (0.5, 1.0) <*> uniformIn (0, 0.3)
+    width <- uniformIn (10, 30)
+    heightMod <- uniformIn (1.5, 3)
+    let height = width * heightMod
+    return . filled color $ polygon [(0, -height), (width / 2, 0), (-width / 2, 0)]
+
+roadsGen :: Double -> Double -> Some [Road]
+roadsGen width height = do
     downCount :: Int <- uniformIn (2, 5)
     rightCount :: Int <- uniformIn (2, 5)
+    downRandom <- listOf downCount $ uniformIn (-15, 15)
+    rightRandom <- listOf rightCount $ uniformIn (-15, 15)
+
+    let perRight = width / (fromIntegral rightCount + 1)
+        perDown = height / (fromIntegral downCount + 1)
+        xShift = width / 2
+        yShift = height / 2
 
     return . snd . runWriter $ do
-        let perRight = width / (fromIntegral rightCount - 1)
-            perDown = height / (fromIntegral downCount - 1)
-            xShift = width / 2
-            yShift = height / 2
+        let getPos shift i per rand = -shift + fromIntegral i * per + rand !! (i - 1)
         tell $ do
-            i <- [0 .. downCount - 1]
-            return $ Road (0, -yShift + fromIntegral i * perDown) (width, 10) grey
+            i <- [1 .. downCount]
+            return $ Road (0, getPos yShift i perDown downRandom) (width, 10) grey
         tell $ do
-            i <- [0 .. rightCount - 1]
-            return $ Road (-xShift + fromIntegral i * perRight, 0) (10, height) grey
+            i <- [1 .. rightCount]
+            return $ Road (getPos xShift i perRight rightRandom, 0) (10, height) grey
 
 
 playerGen :: Some Player
@@ -88,7 +125,7 @@ player = unsafePerformIO $ do
               }
 
 drawCity :: City -> Form
-drawCity City{..} = group $ map drawRoad roads
+drawCity City{..} = group $ map drawRoad roads ++ surroundings
   where
     drawRoad (Road pos size col) = move pos . filled col $ uncurry rect size
 
