9b21eb04af6edfbc1269bc2a835beb4c56561c75
Sandy Maguire <sandy@sandymaguire.me>
2016-03-19 14:49:48 -0700

    eraser works on dynamic signals now

--- src/Game/Combinators.hs
+++ src/Game/Combinators.hs
@@ -3,21 +3,29 @@ module Game.Combinators
     ) where
 
 import Preface
+
+import Control.Arrow (first)
 import Data.List (nub)
 import Game.Sequoia
 import Game.Sequoia.Utils
 
--- TODO(sandy): this stops working if `sps` is not static
--- TODO(sandy): maybe filter by index instead of by eq?
 eraser :: Signal [Prop] -> (Prop -> Bool) -> Signal Prop -> Signal [Prop]
 eraser sps f sdozer =
     let remSig = foldp doze [] ((,) <$> sps <*> sdozer)
-     in (filter . flip notElem) <$> remSig <*> sps
+     in filterOutIdx <$> remSig <*> sps
   where
-    doze (ps, dozer) rem =
-        let hit   = showTrace $ overlapping ps dozer
+    doze :: ([Prop], Prop) -> [Int] -> [Int]
+    doze (ps', dozer) rem =
+        let ps    = filterOutIdx rem ps'
+            hit   = overlapping ps dozer
             dozed = nub $ filter f hit
          in if null dozed
                then rem
-               else nub $ rem ++ dozed
+               else rem ++ idxsOf ps' dozed
+
+    filterOutIdx :: [Int] -> [a] -> [a]
+    filterOutIdx idx = map snd . filter (flip notElem idx . fst) . zip [0..]
+
+    idxsOf :: Eq a => [a] -> [a] -> [Int]
+    idxsOf all which = map fst . filter (flip elem which . snd) $ zip [0..] all
 
