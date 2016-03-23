063e4b6515430cba65e1d07007203aa2d6500d23
Sandy Maguire <sandy@sandymaguire.me>
2016-03-22 19:43:27 -0700

    first step towards quicktime interactions

---
+++ src/RPG/Logic/QuickTime.hs
@@ -0,0 +1,74 @@
+{-# LANGUAGE LambdaCase #-}
+module RPG.Logic.QuickTime
+    ( startQuickTime
+    , fireball
+    , quickTime
+    ) where
+
+import RPG.Core
+import Game.Sequoia.Keyboard
+import Control.Monad.IO.Class (liftIO)
+import System.IO.Unsafe (unsafePerformIO)
+
+(quickTime, quickTimeAddr) = newMailbox $ fireball
+(state, stateAddr) = unsafePerformIO $ mailbox 0
+(startTime, startTimeAddr) = newMailbox 0
+(stateTime, stateTimeAddr) = newMailbox 0
+
+type QuickTime = Signal [Prop]
+
+startQuickTime :: QuickTime -> Signal ()
+startQuickTime qt = do
+    setState 0
+    mail quickTimeAddr qt
+    mail startTimeAddr =<< time
+
+setState :: Int -> Signal ()
+setState s = do
+    mail stateAddr s
+    mail stateTimeAddr =<< time
+
+sinceStarting :: Signal Time
+sinceStarting = (-) <$> time <*> startTime
+
+sinceState :: Signal Time
+sinceState = (-) <$> time <*> stateTime
+
+mashing :: Signal Int
+mashing = countIf id $ keyPress SpaceKey
+
+(fireballCount, fireballAddr) = unsafePerformIO $ mailbox 0
+
+fireball :: QuickTime
+fireball = state >>= \case
+    0 -> do
+        since <- sinceState
+        when (since >= 3) $ do
+            liftIO $ putStrLn "and go!"
+            setState 1
+        return []
+    1 -> do
+        mashing
+        since <- sinceState
+        when (since >= 2) $ do
+            mashed <- mashing
+            liftIO . putStrLn $ show mashed
+            mail fireballAddr mashed
+            setState 2
+        return []
+    2 -> do
+        since <- sinceState
+        fireballs <- fireballCount
+        when (fireballs == 0) $ do
+            liftIO $ putStrLn "done"
+        when (since >= 0.5) $ do
+            liftIO . putStrLn $ "pew pew" ++ show fireballs
+            mail fireballAddr (fireballs - 1)
+            setState 2
+        return []
+
+
+
+
+
+
