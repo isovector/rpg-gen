058dafb121019fd8ce4def0fb5a9b5ed9a312768
Sandy Maguire <sandy@sandymaguire.me>
2016-05-01 21:28:08 -0700

    abstract datatypes over time

--- src/RPG/Collection.hs
+++ src/RPG/Collection.hs
@@ -1,5 +1,6 @@
 module RPG.Collection
-    (
+    ( foldingE
+    , newCollection
     ) where
 
 import Control.Arrow
@@ -9,19 +10,27 @@ import RPG.Core
 import Game.Sequoia.Utils
 import qualified Data.Map as M
 
+-- TODO(sandy): this might be scanlEv
+foldingE :: (a -> b -> b)
+         -> b
+         -> N (B b, a -> IO ())
+foldingE f start = do
+    (es, mb) <- callbackStream
+    b <- loop es start
+    return (b, mb)
+  where
+    loop es val = do
+        val' <- (sample $ nextAll es)
+                >>= return . fmap (foldr f val)
+        e <- planNow $ loop es <$> val'
+        return $ step val e
+
 newCollection :: Ord k
               => Map k v
               -> N ( k -> B (Maybe v)
                    , k -> v -> IO ()
                    )
 newCollection start = do
-    (es, mb) <- callbackStream
-    b <- loop es start
+    (b, mb) <- foldingE (uncurry M.insert) start
     return ((<$> b) . M.lookup, curry mb)
-  where
-    loop es val = do
-        val' <- (sample $ nextAll es)
-                >>= return . fmap (foldr (uncurry M.insert) val)
-        e <- planNow $ loop es <$> val'
-        return $ step val e
 
